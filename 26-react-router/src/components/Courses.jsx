e952fdd7eadd306f27676ca1dce4f8c4d5fb3f7a
95Kiryl <Posakh95@mail.ru>
2024-08-15 22:22:14 +0300

    useLocation, useNavigate

--- 26-react-router/src/components/Courses.jsx
+++ 26-react-router/src/components/Courses.jsx
@@ -1,7 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 import courses from '../data/courses.js';
+import { useState } from 'react';
+
+const sortCourses = (courses, key) => {
+  const sortedCourses = [...courses];
+  sortedCourses.sort((a, b) => a[key] - b[key]);
+  return sortedCourses;
+};
 
 const Courses = () => {
+  const location = useLocation();
+  const query = queryString.parse(location.search);
+  const [sortKey, setSortKey] = useState(query.sort);
+  const [sortedCourses, setSortedCourses] = useState();
   return (
     <>
       <h1>Courses</h1>
