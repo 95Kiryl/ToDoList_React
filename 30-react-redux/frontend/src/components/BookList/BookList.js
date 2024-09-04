13a8ef91068b6f10757c1e24f3362a3d46335b6e
95Kiryl <Posakh95@mail.ru>
2024-09-04 22:33:28 +0300

    Add delete book

--- 30-react-redux/frontend/src/components/BookList/BookList.js
+++ 30-react-redux/frontend/src/components/BookList/BookList.js
@@ -1,7 +1,13 @@
+import { deleteBook } from '../../redux/books/actionCreators';
+import { useDispatch, useSelector } from 'react-redux';
 import './BookList.css';
-import { useSelector } from 'react-redux';
+
 const BookList = () => {
     const books = useSelector((state) => state.books);
+    const dispatch = useDispatch();
+    const handleDeleteBook = (id) => {
+        dispatch(deleteBook(id));
+    }
     return (
         <div className='app-block book-list'>
             <h2>Book List</h2>
@@ -13,6 +19,9 @@ const BookList = () => {
                         <li key={book.id}>
                             <div className='book-info'>
                                 {++i}. {book.title} by {book.autor}
+                            </div>
+                            <div className='book-actions'>
+                                <button onClick={() => handleDeleteBook(book.id)}>Delete</button>
                             </div>
                         </li>
                     ))}
